1e96d7619b67cad2e005418cd8e252a391bddd16
Remn <gameremn@mail.ru>
2020-11-25 20:59:34 +0300

    Начал писать деку

--- Assets/Scripts/CardDeck.cs
+++ Assets/Scripts/CardDeck.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CardDeck : MonoBehaviour
 {
+    public List<Condition> conditions;
+    public List<Card> cards;
+
+    public int chance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (conditions.Count != 0)
+        {
+            foreach (var condition in conditions)
+            {
+                condition.resource.decks.Add(this);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CalculateChance()
     {
-        
+        int newChance = 0;
+        foreach (var condition in conditions)
+        {
+            if (condition.resource.value >= condition.fromResourceNumber &&
+                condition.resource.value <= condition.toResourceNumber)
+            {
+                newChance += condition.trueChance;
+            }
+        }
+
+        chance = newChance;
     }
+}
+
+[System.Serializable]
+public class Condition
+{
+    public Resource resource;
+    [Range(0, 100)]
+    public int fromResourceNumber;
+    [Range(0, 100)]
+    public int toResourceNumber;
+    public int trueChance;
 }
---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public List<CardDeck> decks;
+
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckDecks()
+    {
+        foreach (var deck in decks)
+        {
+            //deck.CheckResource();
+        }
+    }
+}
