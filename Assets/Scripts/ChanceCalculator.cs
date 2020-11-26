f9b261e8c484756b6a277a31015d9c0b618bb4c3
Remn <gameremn@mail.ru>
2020-11-26 09:51:03 +0300

    Начал писать логику карточек

---
+++ Assets/Scripts/ChanceCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class ChanceCalculator
+{
+    public static CardDeck SelectByChance(List<CardDeck> list)
+    {
+        int chanceSum = 0;
+        foreach (var element in list)
+        {
+            chanceSum += element.chance;
+        }
+
+        int elementChance = Random.Range(0, chanceSum);
+        foreach (var element in list)
+        {
+            if (elementChance < element.chance)
+            {
+                return element;
+            }
+            else
+            {
+                elementChance -= element.chance;
+            }
+        }
+        return list[0];
+    }
+    
+    public static Card SelectByChance(List<Card> list)
+    {
+        int chanceSum = 0;
+        foreach (var element in list)
+        {
+            chanceSum += element.chance;
+        }
+
+        int elementChance = Random.Range(0, chanceSum);
+        foreach (var element in list)
+        {
+            if (elementChance < element.chance)
+            {
+                return element;
+            }
+            else
+            {
+                elementChance -= element.chance;
+            }
+        }
+        return list[0];
+    }
+}
